283d73af08007a895b6eddc812c596f6ea68e83a
helloShen <symantec__@hotmail.com>
2022-06-05 04:01:06 -0400

    test route

--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import {
   Box,
   AppBar,
@@ -21,15 +21,15 @@ export default function Navbar() {
               component="div"
               sx={{flexGrow: 1}}
             >
-              <NavLink className="nav-link" to="/mern-employees/">
+              <Link className="nav-link" to="/mern-employees/">
                 <span className="logo">
                   MERN = MongoDB + Express + React + Node
                 </span>
-              </NavLink>
+              </Link>
             </Typography>
-            <NavLink className="nav-link" to="/mern-employees/create">
+            <Link className="nav-link" to="/mern-employees/create">
               <Button color="inherit">Add Record</Button>
-            </NavLink>
+            </Link>
           </Toolbar>
         </AppBar>
       </Box>
