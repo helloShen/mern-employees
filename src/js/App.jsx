b89081d1a2148420ff2ca35ec300c9b27e6e2d22
helloShen <symantec__@hotmail.com>
2022-06-05 04:02:09 -0400

    test route

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<RecordList />} />
+          <Route path="/" exact element={<RecordList />} />
           <Route path="/mern-employees/" element={<RecordList />} />
           <Route path="/mern-employees/create" element={<Create />} />
           <Route path="/mern-employees/edit/:id" exact element={<Edit />} />
